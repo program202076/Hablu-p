0758a87322c90698359c01ef76c33f6b12704ca6
Hablu-p <ek12bd@gmail.com.com>
2022-10-04 16:37:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// var rostGiven = 0;
+// while (rostGiven < 10){
+//    console.log( "rost eat" );
+//    rostGiven++;
+//    console .log(rostGiven);
+// }
+
+//    function boag(num1,num2){
+//       var result = num1-num2;
+//       return result;
+//    }
+//       var total = boag ( 100 , 50 );
+//       console.log(total);
+
+//    function bhageSeas( num1 , num2 ){
+//       var result = num1 % num2 ;
+//       return result; 
+//    }
+//      var total = bhageSeas ( 100 , 50 );
+//      console.log(total);
+
+  
+//      function bhage( num1 , num2 ){
+//       var result = num1 / num2 ;
+//       return result;
+//   }
+//      var total = bhage ( 100 , 50 );
+//      console.log(total); 
+
+   //   function kmToMile(km){
+   //    var mile = km / 1.61 ;
+   //    return mile ;
+   //   }
+    
+   //   var total = kmToMile( 100 );
+   //   console . log( total );
+
+    //  function kmToMile ( km ){
+    //   console . log ( km / 1.61);
+    //  }
+
+    //  kmToMile ( 100 );
+
+    //  function even ( num ){
+    //   console . log ( num % 2 != 0);
+    //  }
+
+    //  even (10) ;
+
+    //  function odd ( num ){
+    //   console . log ( num % 2 == 1);
+    //  }
+
+    //  odd (11) ;
+     
+   //   
+   
+   var factorial = 1;
+   for (var i = 1; i <= 10; i++) {
+       factorial = factorial * i;
+    }
+   console .log(factorial);
+
+
+
+
+
+
+   
